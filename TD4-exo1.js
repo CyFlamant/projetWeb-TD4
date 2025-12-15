4e187bb7532255d3c8c8b3177110c51feff78e75
Cyrille Flamant <cyrille.flamant@gmail.com>
2025-12-15 09:57:09 +0100

    TD4 terminé

--- TD4-exo1.js
+++ TD4-exo1.js
@@ -1,4 +1,4 @@
-var age = 15;
+var age = 66;
 if(age >= 65) {
     console.log("Vous tirez vos revenus de votre pension.");
 }
@@ -9,3 +9,34 @@ else if (age <18) {
     console.log("Vous recevez une allocation");
 }
 else     console.log("La valeur de la variable n'est pas numérique.");
+
+var day = "Lundi";
+switch (day) {
+    default:
+        console.log("Ce jour n'existe pas !");
+        break;
+    case "Lundi":
+    console.log("C'est lundi");
+    if (age <=18) {console.log("Il y a école !");}
+    else if (age <65) console.log("Allez travailler !");
+    else console.log ("Vous pouvez rester au lit");
+    break;
+        case "Mardi":
+    console.log("C'est mardi");
+    break;
+        case "Mercredi":
+    console.log("C'est mercredi");
+    break;
+        case "Jeudi":
+    console.log("C'est jeudi");
+    break;
+        case "Vendredi":
+    console.log("C'est vendredi");
+    break;
+        case "Samedi":
+    console.log("C'est samedi");
+    break;
+        case "Dimanche":
+    console.log("C'est dimanche");
+    break;
+}
